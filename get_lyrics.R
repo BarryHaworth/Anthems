66a2c56974342b795eed9db4d0193dd2f57dd007
BarryHaworth <barryhaworth@gmail.com>
2020-09-05 15:51:50 +1000

    First Commit - files created

---
+++ get_lyrics.R
@@ -0,0 +1,8 @@
+# Step 1: Get the Anthems
+
+#  This program is intended to harvest the lyrics and store them in text files.
+# Lyrics will be taken from:
+# https://lyrics.fandom.com/wiki/LyricWiki:Lists/National_Anthem
+
+# List of Countries:
+
