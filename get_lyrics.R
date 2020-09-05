ebf6a90bc1fa5606bc565b0552a10d11fe8716c7
BarryHaworth <barryhaworth@gmail.com>
2020-09-05 21:57:20 +1000

    Now reading most of the countries

--- get_lyrics.R
+++ get_lyrics.R
@@ -1,4 +1,4 @@
-# Step 1: Get the Anthems
+# Step 1: Get the list of countries and their Anthems
 
 #  This program is intended to harvest the lyrics and store them in text files.
 # Lyrics will be taken from:
@@ -7,6 +7,8 @@
 library(rvest)
 library(dplyr)
 library(rmutil)
+library(openxlsx)
+library(httr)
 
 PROJECT_DIR <- "c:/R/Anthems"
 DATA_DIR    <- "c:/R/Anthems/data"
@@ -18,62 +20,100 @@ url <- 'https://lyrics.fandom.com/wiki/LyricWiki:Lists/National_Anthem'
 webpage <- read_html(url)
 c_list <- html_nodes(webpage,'ol')
 c_data <- html_text(c_list)
-country_list <- strsplit(c_data,"\n ")
+country_list <- strsplit(c_data,"\n")
 print(country_list)
 
 # Put the list into a data frame and tidy it up
 countries <- data.frame(country_list)
-names(countries) <- "country"
-countries$country <- trimws(countries$country)
+names(countries) <- "country"                   # Name the first field country
+countries$country <- trimws(countries$country)  # remove leading and training blanks
+countries$country <- gsub("&","And",countries$country)
+countries$country <- gsub("'","",countries$country)
+
+# Country names to drop
+countries <- subset(countries,country != "Historic Austrian Anthems")
+countries <- subset(countries,country != "Historic German anthems:")
+countries <- subset(countries,country != "China")
+countries <- subset(countries,country != "Historic anthem (until 1963)")
+countries <- subset(countries,country != "Lithuanian SSR (Historic)")
+countries <- subset(countries,country != "Grand Ducal anthem")
+
+# Country names to update.
 
 head(countries)
 
-# Create links to Anthem lyrics
-# Note: probablyu don't need to save them
-countries$anthem    <- paste0('https://lyrics.fandom.com/wiki/National_Anthem:',
-                            gsub(" ","_",countries$country))
-countries$anthem_en <- paste0('https://lyrics.fandom.com/wiki/National_Anthem:',
-                            gsub(" ","_",countries$country),"/en")
-countries$song    <- paste0('https://lyrics.fandom.com/wiki/National_Song:',
-                            gsub(" ","_",countries$country))
-countries$song_en <- paste0('https://lyrics.fandom.com/wiki/National_Song:',
-                            gsub(" ","_",countries$country),"/en")
+# save(countries,file=paste0(DATA_DIR,"/countries.RData"))
 
-save(countries,file=paste0(DATA_DIR,"/countries.RData"))
+
 
 # Get the lyrics
 
-# Test with Australia for an English language version
-url <- countries %>% filter(country == "Australia") %>% select(anthem)
-url <- toString(url)
-webpage <- read_html(url)
-c_list  <- html_nodes(webpage,'.lyricbox')
-lyrics  <- html_text(c_list[1])
-lyrics <- gsub("\n","",lyrics)
-print(lyrics)
-
-# Test with Afganistan for a non-English version
-url <- countries %>% filter(country == "Afghanistan") %>% select(anthem_en)
-url <- toString(url)
-webpage <- read_html(url)
-c_list  <- html_nodes(webpage,'.lyricbox')
-lyrics  <- html_text(c_list[1])
-lyrics <- gsub("\n","",lyrics)
-print(lyrics)
-
 # Next:  Loop through the countries and extract the lyrics.
 
-# for (c in 1:length(countries$country)){
-for (c in 10:15){
+countries["anthem_type"] <- ""
+countries["url"] <- ""
+countries["lyrics"] <- ""
+
+for (c in 1:length(countries$country)){
+# for (c in 26:27){
   print(paste("Reading country number ",c,countries$country[c]))
-  url <- countries$anthem[c]
-  url <- toString(url)
-  webpage <- read_html(url)
-  c_list  <- html_nodes(webpage,'.lyricbox')
-  lyrics  <- html_text(c_list[1])
-  lyrics <- gsub("\n","",lyrics)
-  print(lyrics)
-  countries$lyrics[c] <- lyrics
+  country_ = gsub(" ","_",countries$country[c])
+  address <- 'https://lyrics.fandom.com/wiki/National_'
+  anthem    <- paste0(address,"Anthem:",country_)
+  anthem_en <- paste0(address,"Anthem:",country_,"/en")
+  anthem_english <- paste0(address,"Anthem:",country_,"_(English)")
+  song      <- paste0(address,"Song:",country_)
+  song_en   <- paste0(address,"Song:",country_,"/en")
+  song_english   <- paste0(address,"Song:",country_,"_(English)")
+  if (http_error(anthem_en) == FALSE) {
+    countries$url[c] <- anthem_en
+    countries$anthem_type[c] <- "Anthem, Translated"
+  }
+  else if (http_error(anthem_english) == FALSE){
+    countries$url[c] <- anthem_english
+    countries$anthem_type[c] <- "Anthem, Translated"
+  }
+  else if (http_error(anthem) == FALSE){
+    countries$url[c] <- anthem
+    countries$anthem_type[c] <- "Anthem"
+  }
+  else if (http_error(song_en) == FALSE){
+    countries$url[c] <- song_en
+    countries$anthem_type[c] <- "Song, Translated"
+  }
+  else if (http_error(song_english) == FALSE){
+    countries$url[c] <- song_english
+    countries$anthem_type[c] <- "Song, Translated"
+  }
+  else if (http_error(song) == FALSE){
+    countries$url[c] <- song
+    countries$anthem_type[c] <- "Song"
+  }
+  print(paste(countries$country[c],countries$anthem_type[c],"at URL",countries$url[c]))
+  if (nchar(countries$url[c])>10){
+    url <- countries$url[c]
+    url <- toString(url)
+    webpage <- read_html(url)
+    c_list  <- html_nodes(webpage,'.lyricbox')
+    lyrics  <- html_text(c_list[1])
+    lyrics <- gsub("\n","",lyrics)
+#    print(lyrics)
+    countries$lyrics[c] <- lyrics
+  }
+  else {
+    print(paste(countries$country[c],"Lyrics not found"))
+  }
 }
 
+countries$lyrics <- gsub("([A-Z])", " \\1",countries$lyrics)  # Insert spaces in front of all capital letters.
+countries$lyrics <- trimws(countries$lyrics)  # remove leading and training blanks
+
+# Outstanding problems.
+# Some anthems not found
+# Some anthems do not pick up the english language version when it exists.
+
+countries$anthem_type <- factor(countries$anthem_type)
+summary(countries$anthem_type)
+
 save(countries,file=paste0(DATA_DIR,"/countries.RData"))
+write.xlsx(countries,paste0(DATA_DIR,"/countries.xlsx"))
