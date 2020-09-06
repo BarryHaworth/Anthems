67f8e3aa1f96854e78db9c4791d395c36b6d4573
BarryHaworth <barryhaworth@gmail.com>
2020-09-06 19:08:57 +1000

    First Sentiment - summary for Bing

--- sentiment.R
+++ sentiment.R
@@ -4,6 +4,7 @@
 library(dplyr)
 library(tidytext)
 library(textdata)
+library(tidyr)
 
 PROJECT_DIR <- "c:/R/Anthems"
 DATA_DIR    <- "c:/R/Anthems/data"
@@ -25,11 +26,12 @@ head(countries$lyrics)
 # AFINN: score that runs between -5 and 5
 
 # Split by Words
-all_lyrics <- countries %>% 
+anthem_words <- countries %>% 
   select(country, lyrics) %>%
   unnest_tokens(word, lyrics) 
 
-filtered_lyrics <- all_lyrics %>% anti_join(stop_words)
+# Filter stop words
+anthem_words <- anthem_words %>% anti_join(stop_words)
 
 # Assign Sentiment
 # Download the Sentiment databases
@@ -37,6 +39,18 @@ bing_vector   <- get_sentiments("bing")
 nrc_vector    <- get_sentiments("nrc")
 afinn_vector  <- get_sentiments("afinn")
 
-head(bing_vector)
-head(nrc_vector)
-head(afinn_vector)
+head(bing_vector)  # Positive or Negative
+head(nrc_vector)   # Positive or Negative & others
+head(afinn_vector) # Score
+
+# Assign sentitment using each of the three databases
+anthem_words_bing  <- anthem_words %>% left_join(bing_vector)
+anthem_words_nrc   <- anthem_words %>% left_join(nrc_vector)
+anthem_words_afinn <- anthem_words %>% left_join(afinn_vector)
+
+#  Summarise sentiment by country
+anthem_bing <- anthem_words_bing %>%
+  group_by(country) %>% 
+  count(sentiment) %>% # count the # of positive & negative words
+  spread(sentiment, n, fill = 0) %>% # made data wide rather than narrow
+  mutate(sentiment = positive - negative)
